0842a2fe66d1cda147fa45f5d5cfde7f083eeca2
linesh <linesh.badole@gmail.com>
2024-01-04 15:23:14 +0000

    Section 9 Complete

---
+++ models/fct/fct_reviews.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_shcema_change = 'fail'
+    )
+    
+}}
+
+with src_reviews as 
+(    
+    select * from {{ ref('src_reviews') }}
+)
+
+select * from src_reviews
+where review_txt is not null
+{% if is_incremental() %}
+    and review_date > (select max(review_date) from {{this}} )
+{% endif %}
